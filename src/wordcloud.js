cbe133f0bb3865857c7cb3d506b0589c07bd6650
Hannah Kuehne <hannahsarah.kuehne@students.fhnw.ch>
2019-05-21 22:03:12 +0200

    first implementation of word cloud

--- src/wordcloud.js
+++ src/wordcloud.js
@@ -13,6 +13,42 @@ var svgCloud = d3.select("#wordcloud")
         "translate(" + margin.left + "," + margin.top + ")");
 //*PREPARE DATA*
 // Convert CSV into an array of objects
-/*d3.csv("./data/Swissvote.csv").then(function (data) {
+d3.csv("./data/SwissvoteV2.csv").then(function (data) {
 
-}*/
+    var words = ["Armee", "Umweltpolitik", "Tierschutz", "Ausländerpolitik", "Homosexuelle", "Gentechnologie", "Tierversuche", "Stellung der Frau", "Medienfreiheit", "Flüchtlinge", "Grundrechte"]
+
+
+    var layout = d3.layout.cloud()
+        .size([width, height])
+        .words(words.map(function (d) {
+            return {text: d};
+        }))
+        .padding(5)
+        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .fontSize(30)
+        .on("end", draw);
+    layout.start();
+
+// This function takes the output of 'layout' above and draw the words
+// Better not to touch it. To change parameters, play with the 'layout' variable above
+    function draw(words) {
+        svgCloud
+            .append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", function (d) {
+                return d.size + "px";
+            })
+            .style("font-family", "Arial")
+            .attr("text-anchor", "middle")
+            .attr("transform", function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function (d) {
+                return d.text;
+            });
+        d3.layout.cloud().stop();
+    }
+})
