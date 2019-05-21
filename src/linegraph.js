aed2ce01b64a3dceb2565f873c7c2298e899cdfb
Tabea <tabea.eggler@students.fhnw.ch>
2019-05-21 23:26:29 +0200

    basic linegraph with random numbers

--- src/linegraph.js
+++ src/linegraph.js
@@ -1,29 +1,45 @@
+// set the dimensions and margins of the graph
 var margin = {top: 20, right: 60, bottom: 60, left: 60},
     width = 1200 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 300 - margin.top - margin.bottom;
 
+//example data
 data = [
     {"category": "Sozialpolitik", "total": 110, "angenommen": 40, "abgelehnt": 70},
     {"category": "Umwelt", "total": 30, "angenommen": 12, "abgelehnt": 18},
     {"category": "Staatsordnung", "total": 190, "angenommen": 120, "abgelehnt": 70}
 ];
 
-var line = d3.svg.line()
-    .x(function (d) { return d.total})
-    .y(function (d) { return height - d.total})
-    .interpolate("linear");
+// The number of datapoints
+var n = 21;
 
-var svgline = d3.select("#linegraph")
-    .append("svg")
-    .attr({
-        "width": width,
-        "height": height
-    })
+// X scale
+var xScale = d3.scaleLinear()
+    .domain([0, n-1]) // input
+    .range([0, width]); // output
 
-var path = svg.append("path")
-    .attr({
-        d:line(data),
-        "fill": "none",
-        "stroke": "blue",
-        "stroke-width": "5px"
-    })
+// Y scale -> random generate number
+var yScale = d3.scaleLinear()
+    .domain([0, 1]) // input
+    .range([height, 0]); // output
+
+// random numbers
+var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
+
+// d3's line generator
+var line = d3.line()
+    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator
+
+// Add the SVG to the page
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Append the path, bind the data, and call the line generator
+svg.append("path")
+    .datum(dataset) // Binds data to the line
+    .attr("class", "line")
+    .attr("d", line); // Calls the line generator
