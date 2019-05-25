668024bf8e5337dffc3de28e0b77bc7a49c1af60
Tabea <tabea.eggler@students.fhnw.ch>
2019-05-25 12:36:37 +0200

    added real data and style mountains

--- src/linegraph.js
+++ src/linegraph.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 60, bottom: 60, left: 60},
-    width = 1200 - margin.left - margin.right,
+    width = 1400 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // Add the SVG to the page
@@ -16,61 +16,97 @@ var svgLine = d3.select("#linegraph").append("svg")
 // Convert CSV into an array of objects
 d3.csv("./data/SwissvoteV2.csv").then(function (data) {
 
-    //example data
-    data = [
-        {"category": "Sozialpolitik", "total": 110, "status": "angenommen", "jaAnteil": 70, "neinAnteil": 30},
-        {"category": "Umwelt", "total": 30, "status": "abgelehnt", "abgelehnt": 18, "jaAnteil": 49, "neinAnteil": 51},
-        {"category": "Staatsordnung", "total": 190, "status": "abgelehnt", "abgelehnt": 70, "jaAnteil": 20, "neinAnteil": 80}
-    ];
+    var nested_data = d3.nest()
+        .key(function(d) { return d.d1e1; })
+        .rollup(function(l) { return l.length; })
+        .entries(data);
 
-    //sum all abstimmungen
-    var sum = d3.sum(data, function(d) {
-        return d.total;
+    var bottom_values = [
+        {key: 0, value: 0},
+        {key: 0, value: 3},
+        {key: 0, value: 3},
+        {key: 0, value: 0},
+        {key: 0, value: 7},
+        {key: 0, value: 25},
+        {key: 0, value: 25},
+        {key: 0, value: 0},
+        {key: 0, value: 5},
+        {key: 0, value: 15},
+        {key: 0, value: 0},
+        {key: 0, value: 0}
+    ]
+
+    var mixed_data = new Array();
+    for (i = 0; i < nested_data.length; i++) {
+        mixed_data.push(bottom_values[i]);
+        mixed_data.push(nested_data[i]);
+    }
+    //add last bottom value
+    mixed_data.push({key: 0, value: 0})
+
+
+    //count all abstimmungen
+    var countAll = d3.sum(nested_data, function(d) {
+        return d.value;
     });
+
+
+
 
     //nest data (angneommen + abgelehnt)
     var acceptedData = d3.nest().key(function (d) {
         return d.status;
     })
 
-    // The number of datapoints
-    var n = 21;
-
     // X scale
     var xScale = d3.scaleLinear()
-        .domain([0, n-1]) // input
-        .range([0, width]); // output
-    //svgLine.append("g")
-    //    .attr("transform", "translate(0," + height + ")")
-    //    .call(d3.axisBottom(xScale));
+        .domain([0, mixed_data.length]) // input
+        .range([0, width * 1.28]); // output
 
-
-    // Y scale -> random generate number
+    // Y scale
     var yScale = d3.scaleLinear()
-        .domain([0, 1]) // input
-        .range([height, 0]); // output
-   // svgLine.append("g")
-   //     .call(d3.axisLeft(yScale));
-
-    // random numbers
-    var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
+        .domain([0, d3.max(mixed_data, function(d) { return +d.value; })])
+        .range([ height * 0.7, 0 ]);
+    //svgLine.append("g").call(d3.axisLeft(yScale));
 
     // Append the path, bind the data, and call the line generator
     svgLine.append("path")
-        .datum(dataset) // Binds data to the line
+        .datum(mixed_data) // Binds data to the line
         .attr("class", "line")
         .attr("d", d3.line()
             .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-            .y(function(d) { return yScale(d.y); }) // set the y values for the line generator
+            .y(function(d) { return yScale(d.value); }) // set the y values for the line generator
 
         ); // Calls the line generator
+
+    var xValues = new Array();
+    for(var i = 0; i < nested_data; i++){
+        xValues.push(xValues + 20);
+    }
+
+    console.log(xValues)
+
+    function draw_lines(y, x, line1) {
+        svg.append("line")
+            .attr("class", "linegraph-line")
+            .attr("x1", x)
+            .attr("y1", y)
+            .attr("x2", x)
+            .attr("y2", height)
+
+        svg.append("text")
+            .attr("y", y - 20)
+            .attr("x", x)
+            .text(line1);
+    }
+
 
     var txtTotal = svgLine.append("text")
         .attr("class", "txt-Total")
         .attr("x", 157)
         .attr("y", 0)
         .style("text-anchor", "middle")
-        .text("Total Anzahl Abstimmungen: " + sum);
+        .text("Total Anzahl Abstimmungen: " + countAll);
 
 
     //TODO Checkbox
