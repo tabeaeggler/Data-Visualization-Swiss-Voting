e963f3c227b6a2c2a600f576af5804128c606b74
Hannah Kuehne <hannahsarah.kuehne@students.fhnw.ch>
2019-05-25 11:21:36 +0200

    prepared data for displaying in word cloud

--- src/wordcloud.js
+++ src/wordcloud.js
@@ -15,7 +15,53 @@ var svgCloud = d3.select("#wordcloud")
 // Convert CSV into an array of objects
 d3.csv("./data/SwissvoteV2.csv").then(function (data) {
 
-    var words = ["Armee", "Umweltpolitik", "Tierschutz", "Ausländerpolitik", "Homosexuelle", "Gentechnologie", "Tierversuche", "Stellung der Frau", "Medienfreiheit", "Flüchtlinge", "Grundrechte"]
+
+    var words = ["Armee", "Umweltpolitik", "Ausländerpolitik", "Flüchtlinge", "Stellung der Frau", "Homosexuelle", "Gentechnologie", "AHV", "Tierversuche", "Grundrechte"]
+    
+    //nest data
+    var nestedData1 = Array.from(d3.nest()
+        .key(function (d) {
+            return d.d1e3;
+        })
+        .key(function (d) {
+            return d.titel;
+        }).entries(data))
+
+    var nestedData2 = Array.from(d3.nest()
+        .key(function (d) {
+            return d.d2e3;
+        })
+        .key(function (d) {
+            return d.titel;
+        }).entries(data))
+
+    var nestedData3 = Array.from(d3.nest()
+        .key(function (d) {
+            return d.d3e3;
+        })
+        .key(function (d) {
+            return d.titel;
+        }).entries(data))
+
+
+    //join data
+    var unfilteredData = nestedData1.concat(nestedData2).concat(nestedData3)
+
+    //filter data
+    var filteredData = unfilteredData.filter(function (value, index, arr) {
+        return value.key === "3.21" || value.key === "9.31" || value.key === "10.31" || value.key === "10.32" || value.key === "10.33" || value.key === "10.38" || value.key === "11.41" || value.key === "10.21" || value.key === "11.42" || value.key === "1.62";
+    });
+
+    //join datafields with same category in one object
+    var filteredDataSet = [{key: "3.21", values:[]},{key: "9.31", values:[]}, {key: "10.31", values:[]}, {key: "10.32", values:[]}, {key: "10.33", values:[]}, {key: "10.38", values:[]},{key: "11.41", values:[]}, {key: "10.21", values:[]}, {key: "11.42", values:[]}, {key: "1.62", values:[]} ]
+
+    filteredDataSet.forEach(function (el1, index) {
+        filteredData.forEach(function (el2) {
+            if (el1.key === el2.key) {
+                filteredDataSet[index].values = filteredDataSet[index].values.concat(el2.values)
+            }
+        });
+    });
 
 
     var layout = d3.layout.cloud()
@@ -24,7 +70,9 @@ d3.csv("./data/SwissvoteV2.csv").then(function (data) {
             return {text: d};
         }))
         .padding(5)
-        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .rotate(function () {
+            return ~~(Math.random() * 2) * 90;
+        })
         .fontSize(30)
         .on("end", draw);
     layout.start();
