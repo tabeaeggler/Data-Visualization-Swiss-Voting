989bc53643c751dfa357356e2390f524e40d42f4
Tabea <tabea.eggler@students.fhnw.ch>
2019-05-26 11:36:50 +0200

    Basic layout & linegraph layout

--- src/linegraph.js
+++ src/linegraph.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 200, right: 60, bottom: 100, left: 60},
     width = 1400 - margin.left - margin.right,
-    height = 580 - margin.top - margin.bottom;
+    height = 700 - margin.top - margin.bottom;
 
 // Add the SVG to the page
 var svgLine = d3.select("#linegraph").append("svg")
@@ -203,7 +203,7 @@ d3.csv("./data/SwissvoteV2.csv").then(function (data) {
 
     var txtTotal = svgLine.append("text")
         .attr("x", 660)
-        .attr("y", 350)
+        .attr("y", 420)
         .style("text-anchor", "middle")
         .text("Total Anzahl Vorlagen: " + countAll)
         .attr("class", "txt-total");
